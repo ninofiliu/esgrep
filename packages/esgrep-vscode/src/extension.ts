0d9845fc124468d184484e7df27eddda9efef43f
Nino Filiu <nino.filiu@gmail.com>
2022-06-22 18:47:26 +0200

    wip

--- packages/esgrep-vscode/src/extension.ts
+++ packages/esgrep-vscode/src/extension.ts
@@ -1,11 +1,3 @@
-import * as vscode from "vscode";
-
-export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(
-    vscode.commands.registerCommand("esgrep-vscode.helloWorld", () => {
-      vscode.window.showInformationMessage("Hello World from esgrep-vscode!");
-    })
-  );
-}
+export function activate() {}
 
 export function deactivate() {}
